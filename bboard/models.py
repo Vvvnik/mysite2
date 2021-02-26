a57b55b137a8f30e0a4931b79a2065c09039421a
vvv <vvv-nik@yandex.ru>
2021-02-26 17:19:26 +0300

    Added Db content

--- bboard/models.py
+++ bboard/models.py
@@ -7,3 +7,4 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True)
     price = models.FloatField(null=True, blank=True)
     published = models.DateTimeField(auto_now_add=True, db_index=True)
+
